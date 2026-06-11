3869cad048a6d113fa77b0ad9172dd69815549f7
caiocorrealves15 <caiocorrealves15@gmail.com>
2026-06-11 01:21:24 +0000

    Correções de comandos e ajuste do gitignore

--- index.js
+++ index.js
@@ -936,7 +936,7 @@ if (lowerText.includes('bebida') || lowerText.includes('cerveja') || lowerText.i
 
     // Ache essa linha no seu código e adicione o '!cargos' nela:
 const comandosExistentes = [
-    '!menu', '!comprar', '!loja', '!backup', '!atacar', '!boss', '!rank', '!casar', '!casais', '!piada', '!avisoadm', 
+    '!menu', '!comprar', '!loja', '!backup', '!dar_pontos', '!atacar', '!boss', '!rank', '!casar', '!casais', '!piada', '!avisoadm', 
     '!descasar', '!emoji', '!sortear', '!cadastros', '!perguntas', '!jogar', 
     '!forca', '!jogosoff', '!jogoson', '!link', '!tier', 
     '!ranking', '!penalti', '!musica', '!socar', '!beijar', 
